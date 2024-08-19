1a79ce558ca2c51a57e6a63b9eae34adcaebdc11
jakubSuranUSM <123655266+jakubSuranUSM@users.noreply.github.com>
2024-08-19 16:37:30 +0000

    tests

--- main.c
+++ main.c
@@ -14,18 +14,19 @@ void printArray(int* nums, int numSize) {
 }
 
 void recursion(int* nums, int numSize, int target, int* result) {
-    //printArray(nums, numSize);
+    printf("start");
+    printArray(nums, numSize);
     if (numSize < 3) {
-        *result += numSize == 2 ? nums[0] + nums[1] == target ? 1 : 0 : 0;
-        //printf("result end %d\n", *result);
+        *result += (numSize == 2 && nums[0] + nums[1] == target) ? 1 : 0;
+        printf("result end %d\n", *result);
         return;
     }
-    //printf("Numsize %d, target %d\n", numSize, target);
+    printf("Numsize %d, target %d\n", numSize, target);
 
     int start = nums[0] + nums[1];
     int end = nums[numSize - 1] + nums[numSize - 2];
     int sides = nums[0] + nums[numSize - 1];
-    int resultOne, resultTwo, resultThree = 0;
+    int resultOne = 0, resultTwo = 0, resultThree = 0;
 
     if (start == target) {
         recursion(nums + 2, numSize - 2, target, &resultOne);
@@ -42,25 +43,34 @@ void recursion(int* nums, int numSize, int target, int* result) {
 
     *result += resultOne > resultTwo ? resultOne > resultThree ? resultOne : resultThree : resultTwo > resultThree ? resultTwo : resultThree;
     
-    //printf("result %d\n", *result);
+    printf("end ------- result %d\n", *result);
 }
 
 int maxOperations(int* nums, int numsSize) {
-    int resultOne, resultTwo, resultThree = 0;
+    int resultOne = 0, resultTwo = 0, resultThree = 0;
 
     int targetStart = nums[0] + nums[1];
     int targetEnd = nums[numsSize - 1] + nums[numsSize - 2];
     int targetSides = nums[0] + nums[numsSize - 1];
+
+    printf("result 1 %d\n", resultOne);
+    printf("result 2 %d\n", resultTwo);
+    printf("result 3 %d\n", resultThree);
+
     
     recursion(nums + 2, numsSize - 2, targetStart, &resultOne);
     recursion(nums, numsSize - 2, targetEnd, &resultTwo);
     recursion(nums + 1, numsSize - 2, targetSides, &resultThree);
+    
+    printf("result 1 %d\n", resultOne);
+    printf("result 2 %d\n", resultTwo);
+    printf("result 3 %d\n", resultThree);
 
     return 1 + (resultOne > resultTwo ? resultOne > resultThree ? resultOne : resultThree : resultTwo > resultThree ? resultTwo : resultThree);
 }
 
 int main() {
-    int nums[] = {1, 2, 3, 4, 5};
+    int nums[] = {3,2,6,1,4};
     int numsSize = sizeof(nums) / sizeof(nums[0]);
 
     int maxOps = maxOperations(nums, numsSize);
